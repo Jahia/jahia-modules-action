df771282cd5cd25d3d1fb48e271dd09b33f91b7a
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-12-16 19:58:46 -0500

    Updated the integration tests

--- integration-tests/setup.sh
+++ integration-tests/setup.sh
@@ -4,4 +4,4 @@ echo "Install jahia-reporter"
 npm install -g jahia-reporter
 
 echo "Exporting important environment variables"
-echo "export MANIFEST=${{ inputs.tests_manifest }}" >> $BASH_ENV
+echo "export MANIFEST=${MANIFEST}" >> $BASH_ENV
