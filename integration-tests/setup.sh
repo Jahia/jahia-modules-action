c51a9a916d9bd01d375385277c2fe2ab1d754e18
Francois Gerthoffert <f.gerthoffert@gmail.com>
2021-12-16 20:33:46 -0500

    Updated the integration tests

--- integration-tests/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Install jahia-reporter"
-npm install -g jahia-reporter
-
-# echo "Exporting important environment variables"
-# echo "export MANIFEST=abcd" >> $BASH_ENV
-# echo "{MANIFEST}={MANIFEST}" >> $GITHUB_ENV
