6105b6008b431e7633bb555b58d4f148ea473b65
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-02-04 19:30:51 -0500

    Started to write the action

--- helper/src/main.ts
+++ helper/src/main.ts
@@ -1,8 +1,61 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
+    const checkName = core.getInput('check_name')
+    const pullRequest = github.context.payload.pull_request
+    const commit = core.getInput('commit')
+    const summary = core.getInput('summary')
+    const head_sha =
+      commit || (pullRequest && pullRequest.head.sha) || github.context.sha    
+
+    const token =
+      core.getInput('token') ||
+      core.getInput('github_token') ||
+      process.env.GITHUB_TOKEN;
+
+    if (!token) {
+      core.setFailed('❌ A token is required to execute this action')
+      return
+    }
+
+    core.startGroup(`📘 Display EC2 Metadata`)
+    core.info('TODO')
+    core.endGroup()
+
+    core.startGroup(`📘 SSH Connection instructions`)
+
+    const conclusion = 'success'
+
+    const createCheckRequest = {
+      ...github.context.repo,
+      name: checkName,
+      head_sha,
+      status: 'completed',
+      conclusion,
+      output: {
+        title: 'SSH Connection instructions',
+        summary,
+        annotations: [{
+          path: 'this is a path',
+          start_line:1,
+          end_line:1,
+          annotation_level: 'notice',
+          message: 'This is the message \n \n #Title Markdown \n \n some more text'
+        }]
+      }
+    }
+
+    core.info(`ℹ️ Creating check`)
+    const octokit = github.getOctokit(token)
+
+    await octokit.rest.checks.create(createCheckRequest)
+    core.info('TODO')
+    core.endGroup()
+
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
