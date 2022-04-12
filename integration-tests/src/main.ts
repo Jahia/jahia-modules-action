da81be99d20c6544098b1506b710730f0ae73ba9
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-04-12 19:06:34 -0400

    Updated action

--- integration-tests/src/main.ts
+++ integration-tests/src/main.ts
@@ -3,14 +3,14 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const moduleId: string = core.getInput('module_id')
+    core.info(`Testing module ${moduleId} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
