520cb254752212d030301a1c1440ee4bc886a111
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-04-12 19:23:47 -0400

    Updated action

--- integration-tests/dist/index.js
+++ integration-tests/dist/index.js
@@ -55,6 +55,16 @@ options.listeners = {
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            core.exportVariable('MANIFEST', core.getInput('tests_manifest'));
+            core.exportVariable('JAHIA_IMAGE', core.getInput('jahia_image'));
+            core.exportVariable('JAHIA_LICENSE', core.getInput('jahia_license'));
+            core.exportVariable('JAHIA_CLUSTER_ENABLED', core.getInput('jahia_cluster_enabled'));
+            core.exportVariable('TESTS_IMAGE', core.getInput('tests_image'));
+            core.exportVariable('JCUSTOMER_IMAGE', core.getInput('jcustomer_image'));
+            core.exportVariable('ELASTICSEARCH_IMAGE', core.getInput('elasticsearch_image'));
+            core.exportVariable('NEXUS_USERNAME', core.getInput('nexus_username'));
+            core.exportVariable('NEXUS_PASSWORD', core.getInput('nexus_password'));
+            core.exportVariable('DOCKER_USERNAME', core.getInput('docker_username'));
             const moduleId = core.getInput('module_id');
             core.startGroup('🛠️ Displaying important environment variables and system info');
             core.info(`Testing module ${moduleId} ...`);
--- integration-tests/src/main.ts
+++ integration-tests/src/main.ts
@@ -18,6 +18,23 @@ options.listeners = {
 
 async function run(): Promise<void> {
   try {
+    core.exportVariable('MANIFEST', core.getInput('tests_manifest'))
+    core.exportVariable('JAHIA_IMAGE', core.getInput('jahia_image'))
+    core.exportVariable('JAHIA_LICENSE', core.getInput('jahia_license'))
+    core.exportVariable(
+      'JAHIA_CLUSTER_ENABLED',
+      core.getInput('jahia_cluster_enabled')
+    )
+    core.exportVariable('TESTS_IMAGE', core.getInput('tests_image'))
+    core.exportVariable('JCUSTOMER_IMAGE', core.getInput('jcustomer_image'))
+    core.exportVariable(
+      'ELASTICSEARCH_IMAGE',
+      core.getInput('elasticsearch_image')
+    )
+    core.exportVariable('NEXUS_USERNAME', core.getInput('nexus_username'))
+    core.exportVariable('NEXUS_PASSWORD', core.getInput('nexus_password'))
+    core.exportVariable('DOCKER_USERNAME', core.getInput('docker_username'))
+
     const moduleId: string = core.getInput('module_id')
 
     core.startGroup(
