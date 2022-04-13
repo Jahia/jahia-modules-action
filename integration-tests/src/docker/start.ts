c1af102bbdc7cd05b8184753d55c22d91ba1ba9e
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-04-13 15:26:05 -0400

    Continued int tests

--- integration-tests/src/docker/start.ts
+++ integration-tests/src/docker/start.ts
@@ -11,24 +11,27 @@ export async function startDockerEnvironment(
   if (process.env.GITHUB_WORKSPACE && process.env.TESTS_PATH) {
     core.startGroup('🐋 Starting the Docker environment')
 
-    const startupFile = path.join(
+    const testFolder = path.join(
       process.env.GITHUB_WORKSPACE,
-      process.env.TESTS_PATH,
-      ciStartupScript
+      process.env.TESTS_PATH
     )
 
-    const composeFile = path.join(
-      process.env.GITHUB_WORKSPACE,
-      process.env.TESTS_PATH,
-      dockerComposeFile
-    )
+    const startupFile = path.join(testFolder, ciStartupScript)
+
+    const composeFile = path.join(testFolder, dockerComposeFile)
 
     if (fs.existsSync(startupFile)) {
       core.info(`Starting environment using startup script: ${startupFile}`)
-      await runShellCommands([`bash ${startupFile}`], 'artifacts/startup.log')
+      await runShellCommands(
+        [`(cd ${testFolder}; bash ${startupFile})`],
+        'artifacts/startup.log'
+      )
     } else if (fs.existsSync(composeFile)) {
       core.info(`Starting environment using compose file: ${composeFile}`)
-      await runShellCommands([`docker`], 'artifacts/startup.log')
+      await runShellCommands(
+        [`docker -f ${composeFile} up --abord-on-container-exit`],
+        'artifacts/startup.log'
+      )
     } else {
       core.setFailed(
         `Unable to find environment startup instructions. Could not find startup script (${startupFile}) NOR compose file ${composeFile}`
