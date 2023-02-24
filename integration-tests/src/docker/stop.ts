cf4c761622895387ffb302af37956847c60e84cd
Francois G <f.gerthoffert@gmail.com>
2023-02-24 10:27:15 +0100

    Added method to stop and clear docker containers (#78)

---
+++ integration-tests/src/docker/stop.ts
@@ -0,0 +1,56 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+import {runShellCommands} from '../utils/system'
+
+export async function stopDockerEnvironment(
+  testsFolder: string,
+  loggingMode: string
+): Promise<void> {
+  core.info(`Listing all containers that are still running`)
+  await runShellCommands([`docker ps`], 'artifacts/stop.log', {
+    cwd: testsFolder,
+    ignoreReturnCode: true,
+    loggingMode
+  })
+
+  core.info(`Stopping all running containers`)
+  await exec
+    .getExecOutput('docker', ['ps', '-aq'], {
+      ignoreReturnCode: true,
+      silent: true,
+    })
+    .then(async res => {
+      core.info(`Output of ps -aq ${JSON.stringify(res)}`)
+      const containers = res.stdout.split(/\r?\n/)
+      for (const container of containers.filter(c => c.length > 5)) {
+        core.info(`Stopping container: ${container}`)
+        await exec
+        .getExecOutput('docker', ['stop', container], {
+          ignoreReturnCode: true,
+          silent: true,
+        })
+        .then(res => {
+          if (res.stderr.length > 0 && res.exitCode != 0) {
+            core.info(`Unable to stop container: ${container}`)            
+          }
+          core.info(`Container ${container} stopped`)
+        })
+      }
+    })  
+
+  core.info(`Prunning all images and containers`)
+  await runShellCommands([`docker system prune -a -f`], 'artifacts/stop.log', {
+    cwd: testsFolder,
+    ignoreReturnCode: true,
+    loggingMode
+  })  
+
+  core.info(`Listing all containers running (if any)`)
+  await runShellCommands([`docker ps`], 'artifacts/stop.log', {
+    cwd: testsFolder,
+    ignoreReturnCode: true,
+    loggingMode
+  })
+
+}
