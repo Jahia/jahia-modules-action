4df528da99269dc7938b932ed057388994adc053
Francois G. <f.gerthoffert@gmail.com>
2026-01-25 16:59:15 +0100

    ci: removed pagerduty and use github issues (#305)

--- integration-tests/src/jahia-reporter/githubIncident.ts
+++ integration-tests/src/jahia-reporter/githubIncident.ts
@@ -4,42 +4,40 @@ import * as fs from 'fs'
 
 import {runShellCommands} from '../utils/system'
 
-interface JahiaReporterPagerduty {
+interface JahiaReporterGitHubIncident {
   service: string
-  pdApiKey: string
-  pdReporterEmail: string
-  pdReporterId: string
   googleSpreadsheetId: string
   googleClientEmail: string
   googleApiKey: string
+  githubToken: string
 }
 
-export async function createPagerdutyIncident(
+export async function createGitHubIncident(
   testsPath: string,
   reportType: string,
-  options: JahiaReporterPagerduty
+  options: JahiaReporterGitHubIncident
 ): Promise<any> {
-  let command = 'jahia-reporter pagerduty:incident'
+  let command = 'jahia-reporter github:incident'
 
   if (fs.existsSync(testsPath)) {
     command += ` --sourcePath="${testsPath}"`
     command += ` --sourceType="${reportType}"`
   } else {
     core.info(
-      `ERROR: The following path does not exist: ${testsPath} pagerduty incident will not be based on test results`
+      `ERROR: The following path does not exist: ${testsPath} the github issue will not be based on test results`
     )
     command +=
       ' --incidentMessage="Unable to find test folder, the tests were likely interrupted"'
   }
-  command += ` --pdApiKey="${options.pdApiKey}"`
-  command += ` --pdReporterEmail="${options.pdReporterEmail}"`
-  command += ` --pdReporterId="${options.pdReporterId}"`
-  command += ` --pdTwoStepsAssign`
   command += ` --googleSpreadsheetId="${options.googleSpreadsheetId}"`
   command += ` --googleClientEmail="${options.googleClientEmail}"`
   command += ` --googleApiKey="${options.googleApiKey}"`
   command += ` --googleUpdateState`
-  command += ` --service="${options.service}"`
+
+  command += ` --githubToken="${options.githubToken}"`
+  command += ` --githubRepository="${process.env.GITHUB_REPOSITORY}"`
+
+  command += ` --incidentService="${options.service}"`
   command += ` --sourceUrl="${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}"`
 
   await runShellCommands([command], null, {printCmd: false})
